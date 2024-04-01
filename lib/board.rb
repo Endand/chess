0d325c163062a9ed1eea9c4ce38e174d762309aa
Endand <endand2898@gmail.com>
2024-04-01 18:10:32 -0400

    Added pieces as start pos and put coordinates
    Now the game starts with how real chess
    game looks like in the beginning

--- lib/board.rb
+++ lib/board.rb
@@ -5,31 +5,84 @@ class Board
    attr_reader :game_board
 
   def initialize
-    @game_board = Array.new(8) { Array.new(8) { WHITE_KING } }
+    @game_board = Array.new(8) { Array.new(8) { " " } }
+    chess_pieces_init
+  end
+
+  def chess_pieces_init
+   piece_place('white')
+   piece_place('black')
+  end
+  
+  def piece_place(color)
+   pawn_place(color)
+   special_piece_place(color)
+  end
+
+  def special_piece_place(color)
+   if color=='white'
+      @game_board[7][0] = WHITE_ROOK
+      @game_board[7][7] = WHITE_ROOK
+      @game_board[7][1] = WHITE_KNIGHT
+      @game_board[7][6] = WHITE_KNIGHT
+      @game_board[7][2] = WHITE_BISHOP
+      @game_board[7][5] = WHITE_BISHOP
+      @game_board[7][3] = WHITE_QUEEN
+      @game_board[7][4] = WHITE_KING
+   elsif color=='black'
+      @game_board[0][0] = BLACK_ROOK
+      @game_board[0][7] = BLACK_ROOK
+      @game_board[0][1] = BLACK_KNIGHT
+      @game_board[0][6] = BLACK_KNIGHT
+      @game_board[0][2] = BLACK_BISHOP
+      @game_board[0][5] = BLACK_BISHOP
+      @game_board[0][3] = BLACK_QUEEN
+      @game_board[0][4] = BLACK_KING
+   end
+  end
+
+  def pawn_place(color)
+   if color=='white'
+      8.times do |i|
+         @game_board[6][i] = WHITE_PAWN
+      end
+   elsif color=='black'
+      8.times do |i|
+         @game_board[1][i] = BLACK_PAWN
+      end
+   end
   end
 
   def display
    puts "\nCurrent Board State:\n\n"
    # Display top border
-   puts '+---' * 8 + '+'
-
+   puts ' '+'+---' * 8 + '+'
+ 
    # Display game board
-   @game_board.each_with_index do |row,i|
+   @game_board.each_with_index do |row, i|
+     print 8-i
      print '|'
-     row.each_with_index do |cell,j|
-      if (i + j).even?
-         print "\e[47m"
-      else
-         print "\e[100m"
-      end 
-
-      if cell == ' '
-         print " #{cell} \e[0m|"
+     row.each_with_index do |cell, j|
+       # Determine background color based on row and column index
+       if (i + j).even?
+         print "\e[47m"  # White background
        else
-         print " #{cell} \e[0m|"
+         print "\e[100m" # Grey background
        end
+ 
+       # Print cell content
+       if cell == ' '
+         print " #{cell} "
+       else
+         print " #{cell} "
+       end
+       
+       # Reset background color immediately after printing
+       print "\e[0m|"
      end
-     puts "\n+" + '---+' * 8
+     puts "\n +" + '---+' * 8
    end
+   puts "   A   B   C   D   E   F   G   H"
  end
+ 
 end
