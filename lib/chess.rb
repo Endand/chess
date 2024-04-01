f44f32f1893788ed3672f5cbd0b5c20b0a64a158
Endand <endand2898@gmail.com>
2024-04-01 16:54:21 -0400

    Added display messages and replayability
    Now players can see instructions
    Now players can play again

--- lib/chess.rb
+++ lib/chess.rb
@@ -6,8 +6,57 @@ require_relative './board'
 class Chess
    def play
       greeting
-      
+      player1_name = get_name(1)
+      player2_name = get_name(2)
+      game = Game.new(player1_name, player2_name)
+      game.play_round
+      while play_again?
+         player1_name, player2_name = swap_names(player1_name, player2_name)
+         game = Game.new(player1_name, player2_name)
+         new_game_msg
+         game.play_round
+      end
+      end_message
    end
 
-    
+    def greeting
+      puts "\nWelcome to Connect Four!\n"
+    end
+  
+    def swap_names(name1, name2)
+      temp = name1
+      name1 = name2
+      name2 = temp
+  
+      [name1, name2]
+    end
+  
+    def get_name(player_num)
+      ask_name(player_num)
+      name = gets.strip.capitalize
+    end
+  
+    def ask_name(player_num)
+      puts "\nPlease enter name for player #{player_num}: "
+    end
+  
+    def play_again?
+      ans = ask_again
+      ans == 'y'
+    end
+  
+    def ask_again
+      puts "\nPlay Again? (Y/N)\n"
+      ans = ''
+      ans = gets.strip.downcase[0] until %w[y n].include?(ans)
+      ans
+    end
+  
+    def end_message
+      puts "\nThank you for playing!\n"
+    end
+  
+    def new_game_msg
+      puts "\nNew Game!\n"
+    end
 end
